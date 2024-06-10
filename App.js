b03511ea1a66f4f69f722517354437dc7e6c23fd
robbiedparrish <100664913+robbiedparrish@users.noreply.github.com>
2024-06-10 10:25:00 -0400

    Add files via upload
    SRC folder

---
+++ App.js
@@ -0,0 +1,150 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="huge">Robbie Parrish</h1>
+        <nav className="nav">
+          <a href="https://www.linkedin.com/in/robert-parrish-66ba3a230/">LinkedIn</a>  |  <a href="mailto:robert.parrish@nationalgrid.com">robert.parrish@nationalgrid.com</a>
+        </nav>
+      </header>
+
+      <body>
+        <h1>
+          <strong>Professional Experience</strong></h1>
+        <hr></hr>
+        <h3>
+          National Grid - Waltham MA
+        </h3>
+       
+        <p><i> <b>Associate IT Business Analyst</b> (GDP Rotation 2: US Electric Single Vision Product Team)</i> Jan 2024 - Jul 2024
+          <br></br>Facilitating implementation of an app's MVP release by:
+          <ul>
+            <li> Communicating daily with data engineers and stakeholders to remove blockers for data integration & app configuration.
+            </li>
+            <li>
+              Leading 3 UAT calls with business SMEs and writing 18 live testing scenarios to check configuration and user permissions.
+            </li>
+            <li>
+            Establishing planning materials such as timelines, requirement lists, and field mappings from NG data fields to vendor APIs.
+            </li>
+          </ul>
+        </p>
+
+        <p><i> <b>Associate IT Business Intelligence Analyst</b> (GDP Rotation 1: SRM Data Insights & Analytics Team)</i> May 2023 - Jan 2024
+          <br></br>Progressed data-driven decision making in IT&D by:
+          <ul>
+            <li> 
+             Creating and improving 5 Power BI dashboards with data visualizations and calculations to present insights to executives.
+            </li>
+            <li>
+            Building relationships with IT stakeholders in demo & feedback sessions to understand how to best present their data.            </li>
+            <li>
+            Automating data refresh and file organization with Power Automate and Power Query to enhance 3 dashboards’ upkeep.            </li>
+          </ul>
+        </p>
+
+        <h1>
+          <strong>Education</strong></h1>
+        <hr></hr>
+        <h3>
+          Boston University - Boston MA - May 2023
+        </h3>
+        <p>
+          <i>Bachelor of Arts in Mathematics</i> | Minor: Computer Science 
+          <li>
+          Advanced coursework in Calculus, Statistics, Probability, Differential Equations, Linear Algebra, Data Structures, Algorithms.
+          </li>
+        <li>
+          Drummer in BU Bands & Member of Kappa Kappa Psi service fraternity for college bands.
+          </li>
+          <li>
+          GPA: 3.96/4.00, Summa Cum Laude.
+          </li>
+          
+        </p>
+
+        <h1>
+          <strong>Skills</strong></h1>
+        <hr></hr>
+        <ul>
+          <li>
+          Communication, Presentation, Stakeholder Management, SAFe, Scheduling Leading & Facilitating Meetings.
+          </li>
+          <li>
+          Problem-solving, Analytical Thinking, Mathematics, Data Visualization, Field Mappings & APIs, User Acceptance Testing.
+
+          </li>
+          <li>
+          Microsoft Power BI, Power Automate, Excel, SharePoint, Outlook, Teams, Jira, Confluence.
+
+          </li>
+          <li>
+          Foundational programming logic and familiarity with basics of Python, Java, MATLAB/Julia, JavaScript, HTML, CSS.
+
+          </li>
+          <li>
+          Advanced/Intermediate Spanish Language.
+
+          </li>
+        </ul>
+
+
+        <h1>
+          <strong>Why I'm A Good Fit For OMW Software Testing</strong></h1>
+        <hr></hr>
+        <p>Education:</p>
+        <ul>
+          <li>
+            My Bachelor's in mathematics with a minor in computer science prodives a strong foundation in analyitical thinking and problem-solving skills, which are valuable traits for working in software.
+          </li>
+        </ul>
+
+
+        <p>Experience during my 2nd rotation on Single Vision:</p>
+
+        
+        <ul>
+          <li>
+        My understanding of the electric business and project management skills gained during this rotation are beneficial for a software tester, as they provide domain knowledge and experience understanding a project's requirements.
+
+          </li>
+          <li>
+        My facilitation of meetings between data engineers and business owners demonstrates my ability to collaborate with cross-functional teams, a skill required for software testers.
+
+</li>
+          <li>
+          My involvement in user acceptance testing, where I wrote test steps, delegated roles, and presented feedback, aligns with the skills needed for test planning, execution, and communication as a software tester.
+
+</li>
+        </ul>
+
+
+
+
+        <p>Experience during my 1st rotation on Data Insights & Analytics:</p>
+        
+        <ul>
+          <li>
+          My experience with Power BI and creating data visualization dashboards demonstrates my ability to analyze and present data effectively. This aligns with the analytical and problem-solving skills required for software testing.
+          </li>
+          <li>
+          My interaction with stakeholders, gathering their requirements, and presenting demos showcases my communication and collaboration skills, which are important for software testers.
+          </li>
+          <li>
+          My use of Power Automate to create automations for data file movement and updates indicates my ability to conceptualize how to solve a provlem with automation, which is valuable in a software testing role.
+          </li>
+        </ul>
+
+        
+        
+
+      </body>
+      
+    </div>
+  );
+}
+
+export default App;
